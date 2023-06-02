b215dcde02a0dca67060e904d0209a2934e6ae34
software1engineer1Solomon <suleyman.hacizade1@gmail.com>
2023-06-02 15:40:22 +0400

    this project was learned

---
+++ ui.js
@@ -0,0 +1,24 @@
+function UI() {
+
+    this.outputImage = document.getElementById("outputImage");
+    this.outputLanguage = document.getElementById("outputLanguage");
+    this.outputWord = document.getElementById("outputWord");
+
+    this.languageList = document.getElementById("language");
+
+
+}
+
+UI.prototype.changeUI = function () {
+    // Arayuz Degistirme
+
+    this.outputImage.src = `images/${this.languageList.value}.png`;
+    this.outputLanguage.innerHTML = this.languageList.options[this.languageList.selectedIndex].textContent;
+
+
+
+}
+
+UI.prototype.displayTranslate = function (word) {
+    this.outputWord.textContent = word;
+}
